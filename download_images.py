2185d8f062bd1ad0abde7ac93fd5838068839134
Rodrigo Silveira <rsilveira@deseretdigital.com>
2018-07-12 07:04:41 -0600

    Add image downloader

--- download_images.py
+++ download_images.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 import re
-from PIL import Image
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', help='Input file to parse')
@@ -14,7 +13,6 @@ BASE_PATH = args.directory
 with open(args.file, 'r') as fp:
   for index, line in enumerate(fp.readlines()):
     if index == 0:
-      print('ImageId,Width,Height,PredictionString')
       continue
 
     image, label = line.strip().split(',')
@@ -23,9 +21,15 @@ with open(args.file, 'r') as fp:
 
     if len(filename_parts) == 3:
       image_path = '{}/train/{}/{}.JPEG'.format(BASE_PATH, filename_parts[1], image)
+      if not os.path.exists(image_path):
+        cmd = 'gsutil cp gs://ddm-imagenet/ILSVRC/Data/CLS-LOC/train/{}/{}.JPEG data/train/{}/{}.JPEG'.format(filename_parts[1], image, filename_parts[1], image)
+        os.system(cmd)
+      else:
+        print(' >> Skipping {}'.format(image))
     else:
       image_path = '{}/val/{}.JPEG'.format(BASE_PATH, image)
-
-    if os.path.exists(image_path):
-      im = Image.open(image_path)
-      print('{},{},{},{}'.format(image, im.size[0], im.size[1], label))
+      if not os.path.exists(image_path):
+        cmd = 'gsutil cp gs://ddm-imagenet/ILSVRC/Data/CLS-LOC/val/{}.JPEG data/val/{}.JPEG'.format(image, image)
+        os.system(cmd)
+      else:
+        print(' >> Skipping {}'.format(image))

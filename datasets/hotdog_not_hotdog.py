944ccb3189a408323965ead57cfa7ddf4808afc3
Rodrigo Silveira <rsilveira@deseretdigital.com>
2018-08-07 06:59:52 -0600

    List of foods for a more specialized model

---
+++ datasets/hotdog_not_hotdog.py
@@ -0,0 +1,44 @@
+categories = [
+  # Food that tastes good
+  'pretzel',
+  'chocolate sauce',
+  'ice cream',
+  'French loaf',
+  'dough',
+  'guacamole',
+  'pizza',
+  'cheeseburger',
+  'carbonara',
+  'bagel',
+  'trifle',
+  'ice lolly',
+  'burrito',
+  'potpie',
+  'hotdog',
+  'mashed potato',
+
+  # Fruits
+  'orange',
+  'pineapple',
+  'lemon',
+  'strawberry',
+  'banana',
+  'jackfruit',
+
+  # Boring stuff
+  'bell pepper',
+  'corn',
+  'buckeye',
+  'custard apple',
+  'head cabbage',
+  'spaghetti squash',
+  'acorn',
+  'artichoke',
+  'zucchini',
+  'rotisserie',
+  'consomme',
+]
+
+
+
+
